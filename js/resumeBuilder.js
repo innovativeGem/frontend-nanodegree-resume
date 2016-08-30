c544a89db2ed5f7974139cd290e755f689d7fdc2
Naman Nanavati <naman.nanavati@gmail.com>
2016-08-31 00:21:06 +1000

    Corrections made as per Udacity review

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,13 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var name = "Naman Nanavati";
-var role = "Web Developer";
+
 var bio = {
     "name": "Naman Nanavati",
     "role": "Front End Developer",
     "welcomeMessage": "Seeking an advanced career in Multimedia and Web with consistent growth, by implementing my experience, skills and qualification. Gain management skills and emerge as a leader with management position in Media domain on a global platform.",
-    "bioPic": "images/self.jpg",
+    "biopic": "images/self.jpg",
     "contacts": {
         "email": "naman.nanavati@gmail.com",
         "mobile": "+61 433 433 173",
@@ -34,7 +30,7 @@ var work = {
 };
 
 var projects = {
-    "project": [{
+    "projects": [{
         "title": "BMW Motorrad - All models pricing tool.",
         "dates": "July 2015",
         "description": "I developed this page from stratch to display all BMW Motorrad models and display their prices based on the selected state. When user clicks on any bike, it goes to the details page. It is a single page application using Angular JS template and routing.",
@@ -52,21 +48,23 @@ var education = {
         "name": "Swinburne University",
         "degree": "Master of Multimedia Technology",
         "dates": "2004-2006",
+        "majors": ["Web Development", "Multimedia Project Management", "Web programming", "User Experience Design"],
         "location": "Melbourne, Australia",
-        "info": "http://www.swinburne.edu.au/study/course/Master-of-Multimedia-MA-MM1/international"
+        "url": "http://www.swinburne.edu.au/study/course/Master-of-Multimedia-MA-MM1/international"
     }, {
         "name": "Keyframes Institute of Technology",
         "degree": "Diploma in Multimedia",
         "dates": "2001-2003",
+        "majors": ["Graphic Design", "2D & 3D Animations"],
         "location": "Ahmedabad, India"
     }],
     "onlineCourses": [{
-        "name": "Up and Running with AngularJS 1",
+        "title": "Up and Running with AngularJS 1",
         "school": "Lynda.com",
         "dates": "Jun 2015",
         "url": "https://www.lynda.com/AngularJS-tutorials/Up-Running-AngularJS/154414-2.html"
     }, {
-        "name": "Front-End Web Developer Nanodegree",
+        "title": "Front-End Web Developer Nanodegree",
         "school": "Udacity.com",
         "dates": "Mar 2016 - Oct 2016",
         "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
@@ -76,28 +74,27 @@ var education = {
 //var formattedName = HTMLheaderName.replace("%data%", name);
 //var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-$("#header").append(HTMLbioPic.replace("%data%", bio.bioPic));
-//$("#main").append(internationalizeButton);
-$("#mapDiv").append(googleMap);
-$("#footerContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-$("#footerContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-$("#footerContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-$("#footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
-
 bio.display = function() {
+
+    $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+    $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+    $("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+    $("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+    $("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+    //$("#main").append(internationalizeButton);
+    $("#mapDiv").append(googleMap);
+    $("#footerContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+    $("#footerContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+    $("#footerContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+    $("#footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+
     if (bio.skills !== null) {
         $("#header").append(HTMLskillsStart);
         bio.skills.forEach(function(skill) {
             //console.log(skill);
             $("#skills").append(HTMLskills.replace("%data%", skill));
         });
-    } else {
-        console.log("No skills added");
     }
 };
 
@@ -110,6 +107,7 @@ work.display = function() {
         $("#workExperience").append(HTMLworkStart);
         $(".work-entry:last").append(formattedEmployer + formattedTitle);
         $(".work-entry:last").append(HTMLworkDates.replace("%data%", job.dates));
+        $(".work-entry:last").append(HTMLworkLocation.replace("%data%", job.location));
         $(".work-entry:last").append(HTMLworkDescription.replace("%data%", job.description));
     });
     //console.log(work.jobs[job].title);
@@ -127,7 +125,7 @@ function inName(name){
 projects.display = function() {
     $("#projects").append(HTMLprojectStart);
     //    for (project in projects.project){
-    projects.project.forEach(function(project) {
+    projects.projects.forEach(function(project) {
         $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", project.title));
         $(".project-entry:last").append(HTMLprojectDates.replace("%data%", project.dates));
         $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", project.description));
@@ -142,17 +140,23 @@ education.display = function() {
     $("#education").append(HTMLschoolStart);
     //    for (school in education.schools){
     education.schools.forEach(function(school) {
-        $(".education-entry:last").append(HTMLschoolName.replace("%data%", school.name));
-        $(".education-entry:last").append(HTMLschoolDegree.replace("%data%", school.degree));
+        var schooldegree = HTMLschoolName.replace("%data%", school.name) + HTMLschoolDegree.replace("%data%", school.degree);
+        console.log("schooldegree: "+ schooldegree);
+        $(".education-entry:last").append(schooldegree);
+        //$(".education-entry:last").append(HTMLschoolDegree.replace("%data%", school.degree));
         $(".education-entry:last").append(HTMLschoolDates.replace("%data%", school.dates));
         $(".education-entry:last").append(HTMLschoolLocation.replace("%data%", school.location));
+        school.majors.forEach(function(major){
+            $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", major));
+        });
     });
 
     $(".education-entry:last").append(HTMLonlineClasses);
     //    for (course in education.onlineCourses){
     education.onlineCourses.forEach(function(course) {
-        $(".education-entry:last").append(HTMLonlineTitle.replace("%data%", course.name));
-        $(".education-entry:last").append(HTMLonlineSchool.replace("%data%", course.school));
+        var titleschool = HTMLonlineTitle.replace("%data%", course.title) + HTMLonlineSchool.replace("%data%", course.school);
+        $(".education-entry:last").append(titleschool);
+        //$(".education-entry:last").append(HTMLonlineSchool.replace("%data%", course.school));
         $(".education-entry:last").append(HTMLonlineDates.replace("%data%", course.dates));
         $(".education-entry:last").append(HTMLonlineURL.replace("%data%", course.url));
     });
